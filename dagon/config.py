06661724ab99f87e1e5a97a2cb63be1be045749b
Gianluigi Del Gaudio <gianluigi.delgaudio@studenti.uniparthenope.it>
2024-12-31 11:45:13 +0100

    Fix for Python 3.13 (SafeConfigParser deprecated)

--- dagon/config.py
+++ dagon/config.py
@@ -1,4 +1,4 @@
-from configparser import SafeConfigParser
+from configparser import ConfigParser
 from collections import defaultdict
 
 """
@@ -19,7 +19,7 @@ def read_config(file_config="dagon.ini", section=None):
     :return: dictionary with the configuration
     :rtype: dict(str, dict)
     """
-    config = SafeConfigParser()
+    config = ConfigParser()
     config.read(file_config)
     if section is not None:
         try:
