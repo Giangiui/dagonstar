09a90c6a827bb86c647a3ccac34286b8603af61a
Gianluigi Del Gaudio <gianluigi.delgaudio@studenti.uniparthenope.it>
2025-01-09 10:47:23 +0100

    Fix DagOn error

--- docs/task-multiple-workflows.py
+++ docs/task-multiple-workflows.py
@@ -6,27 +6,25 @@ from dagon import Workflow
 from dagon.dag_tps import DAG_TPS
 from dagon.task import DagonTask, TaskType
 
-workflow = Workflow("English-Writers-Taskflow")
-workflow2 = Workflow("Italian-Writers-Taskflow")
+workflow_eng = Workflow("English-Writers")
+workflow_ita = Workflow("Italian-Writers")
 
-workflow.set_dry(False)  
+workflow_eng.set_dry(False)  
+workflow_ita.set_dry(False)
 
-taskA = DagonTask(TaskType.BATCH, "Hemingway", "echo 10 > A.txt")
-taskB = DagonTask(TaskType.BATCH, "Orwell", "workflow://Italian-Writers-Taskflow/Hemingway/A.txt > B.txt")
+taskA = DagonTask(TaskType.BATCH, "Shakespeare", "echo 10 > A.txt")
+taskB = DagonTask(TaskType.BATCH, "Dante", "cat workflow://English-Writers/Shakespeare/A.txt > B.txt")
 
-workflow.add_task(taskA)
-workflow.add_task(taskB)
-
-taskB.add_dependency_to(taskA)
+workflow_eng.add_task(taskA)
+workflow_ita.add_task(taskB)
 
 metaworkflow=DAG_TPS("WritersDAG")
-metaworkflow.add_workflow(workflow)
-metaworkflow.add_workflow(workflow2)
+metaworkflow.add_workflow(workflow_eng)
+metaworkflow.add_workflow(workflow_ita)
 metaworkflow.make_dependencies()
-
 metaworkflow.run()
 
-if workflow.get_dry() is False:
+if workflow_eng.get_dry() is False and workflow_ita.get_dry() is False:
     
     result_filenameC = taskB.get_scratch_dir() + "/B.txt"
     while not os.path.exists(result_filenameC):
